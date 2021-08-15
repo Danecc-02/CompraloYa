b1aa02e9830b9545e2430066cdfe9c6673e5efa9
Danecc-02 <henrymata02@gmail.com>
2021-08-15 15:21:10 -0600

    Update 1.6

--- CompraloYa/CompraloYa/Migrations/20210815204936_Dejame hacer migraciones... perrrrrroooo.cs
+++ CompraloYa/CompraloYa/Migrations/20210815204936_Dejame hacer migraciones... perrrrrroooo.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace CompraloYa.Migrations
 {
-    public partial class CargaInicial : Migration
+    public partial class Dejamehacermigracionesperrrrrroooo : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -44,6 +44,20 @@ namespace CompraloYa.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_AspNetUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tarjetas",
+                columns: table => new
+                {
+                    VCCtarjeta = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumeroTarjeta = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaExpiracion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarjetas", x => x.VCCtarjeta);
                 });
 
             migrationBuilder.CreateTable(
@@ -202,7 +216,8 @@ namespace CompraloYa.Migrations
                     DetalleJoya = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Stock = table.Column<int>(type: "int", nullable: false),
                     PrecioJoya = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-                    IdSend = table.Column<int>(type: "int", nullable: false)
+                    IdSend = table.Column<int>(type: "int", nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: true)
                 },
                 constraints: table =>
                 {
@@ -226,7 +241,8 @@ namespace CompraloYa.Migrations
                     DetalleOficina = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Stock = table.Column<int>(type: "int", nullable: false),
                     PrecioOficina = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-                    IdSend = table.Column<int>(type: "int", nullable: false)
+                    IdSend = table.Column<int>(type: "int", nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: true)
                 },
                 constraints: table =>
                 {
@@ -250,7 +266,8 @@ namespace CompraloYa.Migrations
                     Stock = table.Column<int>(type: "int", nullable: false),
                     PrecioRopa = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
                     IdRopa = table.Column<int>(type: "int", nullable: false),
-                    IdSend = table.Column<int>(type: "int", nullable: false)
+                    IdSend = table.Column<int>(type: "int", nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: true)
                 },
                 constraints: table =>
                 {
@@ -280,7 +297,8 @@ namespace CompraloYa.Migrations
                     DetalleJoya = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Stock = table.Column<int>(type: "int", nullable: false),
                     PrecioJoya = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-                    IdSend = table.Column<int>(type: "int", nullable: false)
+                    IdSend = table.Column<int>(type: "int", nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: true)
                 },
                 constraints: table =>
                 {
@@ -394,6 +412,9 @@ namespace CompraloYa.Migrations
 
             migrationBuilder.DropTable(
                 name: "Ropas");
+
+            migrationBuilder.DropTable(
+                name: "Tarjetas");
 
             migrationBuilder.DropTable(
                 name: "Tecnologias");
---
+++ CompraloYa/CompraloYa/Models/Tarjetas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompraloYa.Models
+{
+    public class Tarjetas
+    {
+        [Key]
+        public int VCCtarjeta { get; set; }
+        public string NumeroTarjeta { get; set; }
+
+        public string FechaExpiracion { get; set; }
+
+        
+    }
+}
